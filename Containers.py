ff163eb0824d302ad9b4b0e0a9e13236cd427e2f
YongBum <59983323+Yong-Park@users.noreply.github.com>
2021-03-04 08:06:51 -0600

    Add files via upload

--- Containers.py
+++ Containers.py
@@ -1,7 +1,6 @@
 #Algoritmos y Estructuras de Datos, código basado y obtenido de https://simpy.readthedocs.io/en/latest/topical_guides/resources.html#res-type-container
 #Yong Bum Park 20117
 #Maria Isabel solano 20504
-
 import simpy
 import random
 
@@ -12,11 +11,12 @@ class GasStation:
         self.mon_proc = env.process(self.monitor_tank(env))
 
     def monitor_tank(self, env):
-       while True:
-           if self.gas_tank.level < 100:
-               print(f'Calling tanker at {env.now}')
-               env.process(tanker(env, self))
-           yield env.timeout(15)
+        while True:
+            if self.gas_tank.level < 100:
+                print(f'Calling tanker at {env.now}')
+                env.process(tanker(env, self))
+            yield env.timeout(15)
+
 
 def tanker(env, gas_station):
     yield env.timeout(10)  # Need 10 Minutes to arrive
@@ -29,17 +29,18 @@ def car(name, env, gas_station):
     print(f'Car {name} arriving at {env.now}')
     with gas_station.fuel_dispensers.request() as req:
         yield req
+        print("Cantidad de gas que tiene: %s" %(gas_station.gas_tank.level))
         print(f'Car {name} starts refueling at {env.now}')
         yield gas_station.gas_tank.get(40)
         yield env.timeout(5)
         print(f'Car {name} done refueling at {env.now}')
+        print("Cantidad de gas que queda: %s" %(gas_station.gas_tank.level))
 
 
 def car_generator(env, gas_station):
     for i in range(4):
         env.process(car(i, env, gas_station))
         yield env.timeout(5)
-
 
 env = simpy.Environment()
 gas_station = GasStation(env)
