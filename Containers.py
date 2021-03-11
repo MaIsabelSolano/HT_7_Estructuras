be4024d50f94a48ed7385770a5ebd24ef2b3a822
YongBum <59983323+Yong-Park@users.noreply.github.com>
2021-03-10 19:50:07 -0600

    Add files via upload

--- Containers.py
+++ Containers.py
@@ -4,15 +4,15 @@
 import simpy
 import random
 
-class Proceso:
+class GasStation:
     def __init__(self, env):
-        self.fuel_dispensers = simpy.Resource(env, capacity=3)
-        self.RAM = simpy.Container(env, init=100, capacity=1000)
+        self.fuel_dispensers = simpy.Resource(env, capacity=2)
+        self.gas_tank = simpy.Container(env, init=100, capacity=100)
         self.mon_proc = env.process(self.monitor_tank(env))
 
     def monitor_tank(self, env):
         while True:
-            if self.RAM.level < 100:
+            if self.gas_tank.level < 100:
                 print(f'Calling tanker at {env.now}')
                 env.process(tanker(env, self))
             yield env.timeout(15)
@@ -21,38 +21,46 @@ class Proceso:
 def tanker(env, gas_station):
     yield env.timeout(10)  # Need 10 Minutes to arrive
     print(f'Tanker arriving at {env.now}')
-    amount = gas_station.RAM.capacity - gas_station.RAM.level
-    yield gas_station.RAM.put(amount)
+    amount = gas_station.gas_tank.capacity - gas_station.gas_tank.level
+    yield gas_station.gas_tank.put(amount)
 
 
-def proceso(name, env, gas_station):
-    #env, name, bcs, driving_time, charge_duration
+def car(name, env, gas_station):
     global totalTime
     arrivingTime = env.now
-    print(f'Proceso {name} llegando en {env.now}')
+    print(f'Proceso {name} arriving at {env.now}')
     with gas_station.fuel_dispensers.request() as req:
         yield req
-        print("Cantidad de memoria que tiene: %s" %(gas_station.RAM.level))
+        print("Cantidad de gas que tiene: %s" %(gas_station.gas_tank.level))
         print(f'gas_station {name} starts refueling at {env.now}')
-        yield gas_station.RAM.get(40)
+        yield gas_station.gas_tank.get(40)
         yield env.timeout(5)
         print(f'gas_station {name} done refueling at {env.now}')
-        print("Cantidad de gas que queda: %s" %(gas_station.RAM.level))
+        print("Cantidad de gas que queda: %s" %(gas_station.gas_tank.level))
     leavingTime = env.now-arrivingTime
     totalTime = totalTime + leavingTime
     print("Tiempo total transcurido: %s" %totalTime)
+#es el que decide cuantas veces lo realizara
+cantidad =25
 
-
-def proceso_generator(env, gas_station):
+def car_generator(env, gas_station):
     
-    for i in range(25):
-        env.process(proceso(i, env, gas_station))
-        #env.process(car(env, 'Car %d' % i, bcs, random.expovariate(1.0/10), tcarga))
+    for i in range(cantidad):
+        env.process(car(i, env, gas_station))
+        
         yield env.timeout(5)
 
-#run simulation
 env = simpy.Environment()
-gas_station = Proceso(env)
-random.seed(10)
-proceso_gen = env.process(proceso_generator(env, gas_station))
+gas_station = GasStation(env)
+car_gen = env.process(car_generator(env, gas_station))
 totalTime=0
+env.run(10000)
+#promedio de atencion por carro. 
+print("Tiempo total: %s" %totalTime)
+promedio = totalTime/cantidad
+print("Su promedio es de: %s" %promedio)
+
+
+
+
+
